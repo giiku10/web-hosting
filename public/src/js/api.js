49908b31a48a928e9786929c3197cf2219404bc4
YoshihisaTanaka-prog <nakanaka.tanaka4413@gmail.com>
2023-10-14 18:00:09 +0900

    firebaseの変更

--- public/src/js/api.js
+++ public/src/js/api.js
@@ -1,7 +1,7 @@
 function sendData(id) {
   let value = $("#question-input-" + id).val();
-  alert(value);
-  $.post(apiUrl, {questionId: id, value: value}, function(data){
+  alert(value + " " + keyData[id]);
+  $.post(apiUrl + '/sendValue', {classId: classId, questionId: keyData[id], value: value}, function(data){
     console.log(data);
   });
 }
--- public/src/js/view.js
+++ public/src/js/view.js
@@ -1,16 +1,20 @@
 var shownStatus = {};
+var keyData = {};
+var currentKey = "";
 
 function writePage(data){
   $("#main").html("");
   $("#main").append("<h1>" + data.name + "</h1>");
   $("#main").append("<ul id='main-questions'></ul>");
   let id = "#main-questions"
-  for(let question of data.questions){
-    let key = Object.keys(question)[0];
-    var questionData = question[key]
+  for(let key of sortedKeys(data.questions)){
+    var questionData = data.questions[key];
     questionData["id"] = key;
+    currentKey = key;
+    keyData[key] = key;
     writeQuestion(questionData, id);
   }
+  console.log(keyData);
 }
 
 function writeQuestion(data, id){
@@ -26,10 +30,12 @@ function writeQuestion(data, id){
   }
   if(data.children){
     childId = "#question-ul-" + data.id;
-    for(let question of data.children){
-      let key = Object.keys(question)[0];
-      var questionData = question[key]
+    let parentKey = currentKey;
+    for(let key of sortedKeys(data.children)){
+      var questionData = data.children[key]
       questionData["id"] = key;
+      currentKey = parentKey + "-" + key;
+      keyData[key] = currentKey;
       writeQuestion(questionData, childId);
     }
   } else{
@@ -46,4 +52,20 @@ function changeShownStatus(id){
     shownStatus[id] = true;
     $("#question-ul-" + id).css("display", "block");
   }
+}
+
+function sortedKeys(questions) {
+  var keys = Object.keys(questions);
+  for (var i = 0; i < keys.length - 1; i++){
+    for (let j = 0; j < keys.length - i - 1; j++) {
+      let key = keys[j];
+      let keyPlus = keys[j+1];
+      if( questions[key].name > questions[keyPlus].name ){
+        let keep = keys[j];
+        keys[j] = keys[j+1];
+        keys[j+1] = keep;
+      }
+    }
+  }
+  return keys;
 }
